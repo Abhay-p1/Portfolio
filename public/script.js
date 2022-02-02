5fa94915d45404c65f5d717b890a48c0323e0426
Abhay-p1 <abhayaj806@gmail.com>
2022-02-02 13:04:01 +0530

    portfoio

--- public/script.js
+++ public/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings:  ["Developer", "Competitive Programmer", "Open Source Enthusiast"],
+        strings:  ["Developer", "Competitive Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Competitive Programmer", "Open Source Enthusiast"],
+        strings: ["Developer", "Competitive Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
--- server.js
+++ server.js
@@ -8,5 +8,5 @@ const PORT = process.env.PORT || 4444
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, (req, res) => {
-    console.log(`started on http://localhost:${PORT}`)
+    console.log(`started on http://localhost:${PORT}`);
 })
